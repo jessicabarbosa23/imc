eaaa96329169005a65ac7ebff0431349e51e7ee7
jessicabarbosa23 <jessica.bb.machado@gmail.com>
2025-04-30 11:28:09 -0300

    imc.js

--- imc.js
+++ imc.js
@@ -13,11 +13,11 @@ function imc(){
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
-            if (imc < 18.5 ) this.classificacao = "obesidade mÃ³rbida";
-            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "sauda¡vel"
+            if (imc < 18.5 ) this.classificacao = "obesidade mórbida";
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudavel"
             if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
-            if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
+            if (imc >= 40 &&  imc < 45 ) this.classificacao = "obesidade Severa"
             if (imc >= 40 ) this.classificacao = "magreza"
             return imc.toFixed(2);
         
@@ -29,5 +29,5 @@ function imc(){
     console.log(paciente)
     console.log(paciente.calcularIMC())
     let resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificação de peso Ã© " + paciente.classificacao;
+    resposta.innerHTML="Seu IMC é "+paciente.calcularIMC()+". " + "Sua classificação de peso " + paciente.classificação;
 }
